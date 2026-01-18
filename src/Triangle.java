dad281dc132edda9a911172a157fc2b5154aabad
amrita-m-pillai <amritampillai06@gmail.com>
2026-01-18 20:11:26 +0530

    first commit

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,17 @@
-public class Triangle {
-    //Add your programs here
-}
+import java.util.Scanner;
+
+	public class Triangle
+		{
+			public static void main(String [] args)
+			{
+				Scanner sc=new Scanner(System.in);
+				for(int i=0;i<3;i++)
+				{
+					for(int j=0;j<2*i+1;j++)
+					{
+						System.out.print("*");
+					}
+				
+				System.out.println("");}
+			}
+		}
